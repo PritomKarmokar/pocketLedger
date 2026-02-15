5b31adc506678bfcfb70ec91e2e590bc9ba657f0
Pritom Karmokar <pritomkarmokar99@gmail.com>
2026-02-16 01:50:57 +0600

    feat(add): accounts admin part

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    # Field showing when creating an user
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "username",
+                "email",
+                "password1",
+                "password2",
+                "is_staff",
+                "is_superuser",
+                "is_active",
+            )
+        }),
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        ("Authentication", {"fields": ("auth_provider",)}),
+    )
+    list_display = ("username", "email", "is_staff", "is_active")
+    ordering = ("email",)
+    search_fields = (
+        "email",
+        "username"
+    )
